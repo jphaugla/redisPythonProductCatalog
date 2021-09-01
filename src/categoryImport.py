88ce4befcf2d56773f059d67758b874ef18c998a
Jason Haugland <jason@Jasons-MacBook-Pro.local>
2021-09-01 12:54:30 -0500

    make new commit as previous github was messed up

---
+++ src/categoryImport.py
@@ -0,0 +1,71 @@
+import xml.etree.ElementTree as ET
+import redis
+
+from Category import Category
+
+REDIS_HOST = 'redis'
+
+
+def main():
+    conn = redis.StrictRedis(REDIS_HOST, charset="utf-8", decode_responses=True)
+    with open('/data/CategoriesList.xml') as xml_file:
+        # create element tree object
+        tree = ET.parse(xml_file)
+
+        # get root element
+        root = tree.getroot()
+        print("root.tag is ", root.tag)
+        cat_cntr = 0
+        for child in root:
+            print("child tag is " + child.tag)
+            print("child attribute is " + str(child.attrib))
+        for cat in root.findall('Response/CategoriesList/Category'):
+            # print("starting in xml file")
+            # print("cat.tag is " + str(cat.tag))
+            # print("cat.attribute is " + str(cat.attrib))
+            next_category = Category()
+            cat_cntr += 1
+            cat_id = cat.attrib['ID']
+            # print("ID is ", str(cat_id))
+            next_category.ID = str(cat_id)
+            # print("ID is " + next_category.ID)
+            next_category.Score = str(cat.attrib['Score'])
+            # category_id = 'categ:' + cat_id
+            # conn.hset(category_id, "ID", cat_id)
+            if cat.attrib['LowPic']:
+                #     conn.hset(category_id, "lowpic", cat.attrib['LowPic'])
+                next_category.LowPic = str(cat.attrib['LowPic'])
+                # print("lowpic is " + next_category.LowPic)
+            if cat.attrib['ThumbPic']:
+                #     conn.hset(category_id, "thumbpic", cat.attrib['ThumbPic'])
+                next_category.ThumbPic = str(cat.attrib['ThumbPic'])
+                # print("thumbpic is " + next_category.ThumbPic)
+            for cat_child in cat:
+                # category_id is
+                # print("cat_child.tag is " + str(cat_child.tag))
+                # print("cat_child.attribute is " + str(cat_child.attrib))
+                if cat_child.tag == 'Name' and cat_child.attrib['langid'] == '1':
+                    cat_name = cat_child.attrib['Value']
+                    # print("category name is " + cat_name)
+                    next_category.set_category_name(cat_name)
+                    # print("category name is " + next_category.Name)
+                elif cat_child.tag == 'ParentCategory' and cat_id != "1":
+                    parent_cat_id = cat_child.attrib['ID']
+                    next_category.ParentCategoryID = parent_cat_id
+                    # print("parent_cat_id is " + parent_cat_id)
+                    for parent_child in cat_child:
+                        # print("parent_child is " + str(parent_child))
+                        for name in parent_child:
+                            # print("name under parent child is " + str(name))
+                            if name.tag == 'Name' and name.attrib['langid'] == '1':
+                                next_category.ParentCategoryName = name.attrib['Value']
+                    conn.hset(next_category.get_key(), mapping=next_category.__dict__)
+            if cat_cntr % 1000 == 0:
+                print(str(cat_cntr) + " categories loaded")
+
+    xml_file.close()
+    print(str(cat_cntr) + " categories loaded")
+
+
+if '__main__' == __name__:
+    main()
---
+++ src/productImport.py
@@ -0,0 +1,55 @@
+import csv
+import redis
+import sys
+import datetime
+
+from Product import Product
+
+maxInt = sys.maxsize
+
+REDIS_HOST = 'redis'
+
+
+def main():
+    # global redis_pool
+    # print("PID %d: initializing redis pool..." % os.getpid())
+    # redis_pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    print("Starting productimport.py at " + str(datetime.datetime.now()))
+    conn = redis.StrictRedis(host=REDIS_HOST, port=6379, db=0, charset="utf-8", decode_responses=True)
+    #  open the file to read as csv
+    with open('/data/files.index.csv') as csv_file:
+        # file is tab delimited
+        csv_reader = csv.DictReader(csv_file, delimiter='\t', quoting=csv.QUOTE_NONE)
+        prod_idx = 0
+        #  go through all rows in the file
+        for row in csv_reader:
+            #  increment prod_idx and use as incremental part of the key
+            prod_idx += 1
+            nextProduct = Product(**row)
+            category_id = 'Category:' + nextProduct.catid
+            categ_name = conn.hget(category_id, "Name")
+            nextProduct.set_category_name(categ_name)
+            nextProduct.set_key()
+            # print("before write of product " + str(nextProduct.product_id) + " " + nextProduct.key_name)
+            conn.hset(nextProduct.key_name, mapping=nextProduct.__dict__)
+            # 0)path 1)product_id 2)updated 3)quality 4)supplier_id 5)prod_id 6)catid 7)m_prod_id 8)ean_upc 9)on_market
+            # 10)country_market 11)model_name 12)product_view 13)high_pic 14)high_pic_size
+            # 15)high_pic_width 16)high_pic_height 17)m_supplier_id 18)m_supplier_name 19)ean_upc_is_approved
+            # 20)Limited Date_Added
+            # index will be model name and hold value of prod_idx
+            if categ_name and not categ_name.isspace():
+                conn.sadd("CategIDX:" + categ_name, nextProduct.key_name)
+            if nextProduct.model_name and not nextProduct.model_name.isspace():
+                model_key: str = "ProductModel:" + nextProduct.model_name
+                # print("model key is " + model_key)
+                conn.sadd(model_key, nextProduct.key_name)
+            if prod_idx % 10000 == 0:
+                print(str(prod_idx) + " rows loaded")
+        csv_file.close()
+        print(str(prod_idx) + " rows loaded")
+        conn.set("prod_highest_idx", prod_idx)
+    print("Finished productimport.py at " + str(datetime.datetime.now()))
+
+
+if '__main__' == __name__:
+    main()
